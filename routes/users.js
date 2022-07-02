f68d6496aeb1b3b8d4946711c62e012bedf58e1a
K-Jonin <jonin.0803@gmail.com>
2022-07-02 17:48:04 +0900

    ユーザー情報の取得・更新・削除処理の追加

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,53 @@
+const e = require("express");
+const User = require("../models/User");
+
 const router = require("express").Router();
-const user = require("../models/User");
+
+// CRUD
+// ユーザー情報の更新
+router.put("/:id", async (req, res) => {
+	if(req.body.userId === req.params.id || req.body.isAdmin) {
+		try{
+			const user = await User.findByIdAndUpdate(req.params.id, {
+				$set: req.body
+			});
+			res.status(200).json("ユーザー情報が更新されました");
+		}
+		catch(err){
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("自らのアカウントのみ更新が可能です");
+	}
+});
+
+// ユーザー情報の削除
+router.delete("/:id", async (req, res) => {
+	if(req.body.userId === req.params.id || req.body.isAdmin) {
+		try{
+			const user = await User.findByIdAndDelete(req.params.id);
+			res.status(200).json("ユーザー情報が削除されました");
+		}
+		catch(err){
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("自らのアカウントのみ削除が可能です");
+	}
+});
+
+// ユーザー情報の取得
+router.get("/:id", async (req, res) => {
+		try{
+			const user = await User.findById(req.params.id);
+			const {password, updatedAt, ...other} = user._doc;
+			res.status(200).json(other);
+		}
+		catch(err){
+			return res.status(500).json(err);
+		}
+		return res.status(403).json("自らのアカウントのみ削除が可能です");
+});
 
 // router.get("/", (req, res) => {
 // 	res.send("user router");
