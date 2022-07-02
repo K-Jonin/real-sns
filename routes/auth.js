9b15ecfde57f58ff1480968171bccc7064cb4e10
K-Jonin <jonin.0803@gmail.com>
2022-07-02 17:27:40 +0900

    ログイン機能の追加

--- routes/auth.js
+++ routes/auth.js
@@ -17,6 +17,22 @@ router.post("/register", async (req, res) => {
 	}
 });
 
+// ログイン
+router.post("/login", async (req, res) => {
+	try{
+		const user = await User.findOne({ email: req.body.email });
+		if(!user) return res.status(404).send("ユーザーが見つかりません");
+
+		const valiedPassword = req.body.password === user.password;
+		if(!valiedPassword) return res.status(400).json("パスワードに誤りがあります");
+
+		return res.status(200).json(user);
+	}
+	catch(err){
+		return res.status(500).json(err);
+	}
+});
+
 
 // router.get("/", (req, res) => {
 // 	res.send("auth router");
