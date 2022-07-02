a22916ea4c989fad7ec9bc4564ae6da2cadde84f
K-Jonin <jonin.0803@gmail.com>
2022-07-03 00:15:16 +0900

    投稿系CRUD処理

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # real-sns
+
+##　使用したライブラリ
+・express
+
+・mongoose
+
+・nodemon
+
+・helmet
+
+ip2ATRDHYmq0ovWG
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,61 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-	res.send("posts router");
+// 投稿を作成する
+router.post("/", async (req, res) => {
+	const newPost = new Post(req.body);
+	try{
+		const savePost = await newPost.save();
+		return res.status(200).json(savePost);
+	}
+	catch(err){
+		return res.status(500).json(err);
+	}
+});
+
+// 投稿を編集する
+router.put("/:id", async (req, res) => {
+		try{
+			const post = await Post.findById(req.params.id);
+			if(post.userId === req.body.userId){
+				await post.updateOne({
+					$set: req.body,
+				});
+				return res.status(200).json("投稿の編集に成功しました");
+			} else {
+				return res.status(403).json("他の人の投稿を編集できません");
+			}
+		}
+		catch(err){
+			return res.status(500).json(err);
+		}
+});
+
+// 投稿を削除する
+router.delete("/:id", async (req, res) => {
+	try{
+		const post = await Post.findById(req.params.id);
+		if(post.userId === req.body.userId){
+			await post.deleteOne();
+			return res.status(200).json("投稿の削除に成功しました");
+		} else {
+			return res.status(403).json("他の人の投稿を削除できません");
+		}
+	}
+	catch(err){
+		res.status(500).json(err);
+	}
+});
+
+// 特定の投稿を取得する
+router.get("/:id", async (req, res) => {
+	try{
+		const post = await Post.findById(req.params.id);
+		return res.status(200).json(post);
+	}
+	catch(err){
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
