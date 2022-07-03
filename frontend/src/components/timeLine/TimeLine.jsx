b91483316685d65280d37cc65a7bc46037a379fc
K-Jonin <jonin.0803@gmail.com>
2022-07-03 19:31:59 +0900

    各コンポーネント作成

---
+++ frontend/src/components/timeLine/TimeLine.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from "../post/Post"
+import Share from "../Share/Share"
+import "./TimeLine.css"
+
+export default function TimeLine() {
+  return (
+	<div className="timeLine">
+    <div className="timeLineWrapper">
+      <Share />
+      <Post />
+    </div>
+  </div>
+  )
+}
